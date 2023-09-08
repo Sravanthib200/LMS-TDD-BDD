c480c59af3d222f4699c8e302d315b5caf3f2071
diptishikhasingh <shaileshverma@shaileshs-mbp>
2023-09-08 01:03:14 -0500

    First Commit

---
+++ src/main/java/LMS_TDD_BDD/LMS_TDD_BDD/App.java
@@ -0,0 +1,13 @@
+package LMS_TDD_BDD.LMS_TDD_BDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
